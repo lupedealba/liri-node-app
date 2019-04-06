0f351bc88314f940b96bc3adc3dc469f2665ddc1
Loopy Muchacha <lupegdealba@gmail.com>
2019-04-06 02:08:06 -0700

    first Commit

---
+++ liriApp.js
@@ -0,0 +1,8 @@
+// https://developer.spotify.com/
+// developer.spotify.com
+
+// Make an account and register an API key
+//https://developer.spotify.com/documentation/web-api/
+
+// Mohammed's liri app
+//https://github.com/mohamedsamara/Liri-Node-App
